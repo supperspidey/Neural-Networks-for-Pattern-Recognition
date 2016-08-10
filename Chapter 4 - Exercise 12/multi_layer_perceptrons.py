3440e192be135e9a30149044f327ec24a7fdfdee
Van Nguyen <van.le.nguyen1991@gmail.com>
2016-08-09 23:06:27 -0400

    Added a method for performing forward propagation.

--- Chapter 4 - Exercise 12/multi_layer_perceptrons.py
+++ Chapter 4 - Exercise 12/multi_layer_perceptrons.py
@@ -4,11 +4,53 @@ import numpy as np
 
 class MultiLayerPerceptrons:
 
-    def __init__(self, numIns, numHiddens):
+    def __init__(self, numIns, numHiddens, numOuts):
         # The extra units are for biases and perminently set to 1
-        self.inputDimension = numIns + 1
-        self.numberOfHiddenUnits = numHiddens + 1
-        self.firstLayerWeights = np.ones(
-            self.inputDimension * numHiddens
+        self.numOuts = numOuts
+        self.numIns = numIns + 1
+        self.numHiddens = numHiddens + 1
+        self.firstLayerWeights = np.random.standard_normal(
+            self.numIns * numHiddens
         )
-        self.secondLayerWeights = np.ones(self.numberOfHiddenUnits)
+        self.secondLayerWeights = np.random.standard_normal(
+            self.numHiddens
+        )
+        self.hiddenUnits = np.zeros(numHiddens)
+        self.hiddenUnits = np.append(1, self.hiddenUnits)
+        self.outputs = np.zeros(numOuts)
+
+    def forwardPropagate(self, x):
+        x_b = np.append(1, x)
+        j = 1
+        k = 0
+        stride = 0
+
+        for i in range(0, self.numIns):
+            while stride != self.firstLayerWeights.size:
+                self.hiddenUnits[j] += (self.firstLayerWeights[i+stride] * x_b[i])
+                j += 1
+                stride += self.numIns
+            j = 1
+            stride = 0
+        self.hiddenUnits[1:self.numHiddens] = self.__activateHiddenUnits()
+
+        for j in range(0, self.numHiddens):
+            while stride != self.secondLayerWeights.size:
+                self.outputs[k] += (self.secondLayerWeights[i+stride] * self.hiddenUnits[j])
+                k += 1
+                stride += self.numHiddens
+            k = 0
+            stride = 0
+        self.outputs = self.__activateOutputUnits()
+
+    def __activateHiddenUnits(self):
+        return 1 - np.square(
+            np.tanh(self.hiddenUnits[1:self.numHiddens])
+        )
+
+    def __activateOutputUnits(self):
+        return self.outputs
+
+
+nn = MultiLayerPerceptrons(2, 5, 2)
+nn.forwardPropagate(np.array([2, 3]))
